6520f7c7870617dad524217f21cc2775a94d9540
David Jazbec <david.jazbec@outlook.com>
2024-03-29 00:20:57 +0100

    UI improvements for new functionality

--- Badgernet.Umbraco.TestingProject/App_Plugins/Badgernet.Umbraco.WebPicAuto/Backoffice/WpaDashboard.js
+++ Badgernet.Umbraco.TestingProject/App_Plugins/Badgernet.Umbraco.WebPicAuto/Backoffice/WpaDashboard.js
@@ -4,15 +4,18 @@ function WebPicAutoController($scope, $http, umbRequestHelper) {
 
     function init() {
 
+        //$scope.$watch(function (){ return vm.allImages;},function (){ console.log("collection changed")});
 
-        umbRequestHelper.resourcePromise(
-            $http.get(baseApiUrl + "GetSettings")
-        ).then(function (data) {
-            vm.settings = JSON.parse(data);
-            vm.sizeSavedConverting = humanFileSize(vm.settings.WpaBytesSavedConverting);
-            vm.sizeSavedResizing = humanFileSize(vm.settings.WpaBytesSavedResizing);
-        });
+        //Retrieves settings from server on load
+        umbRequestHelper.resourcePromise($http.get(baseApiUrl + "GetSettings"))
+            .then(function (data) {
+                    vm.settings = JSON.parse(data);
+                    vm.sizeSavedConverting = humanFileSize(vm.settings.WpaBytesSavedConverting);
+                    vm.sizeSavedResizing = humanFileSize(vm.settings.WpaBytesSavedResizing);
+                }
+            );
 
+        //Sends settings to server for saving
         vm.commit = function(){
             umbRequestHelper.resourcePromise(
                 $http.post(baseApiUrl + "SetSettings",JSON.stringify(vm.settings))
@@ -20,30 +23,44 @@ function WebPicAutoController($scope, $http, umbRequestHelper) {
                 alert(response);
             });
         };
+
+        vm.getAllImages = function (){
+            umbRequestHelper.resourcePromise( $http.get(baseApiUrl + "GetAllImages") )
+                .then(function(data){
+                    vm.allImages = JSON.parse(data);
+                });
+        }
         
+        vm.checkMedia = function (){
+            umbRequestHelper.resourcePromise( $http.get(baseApiUrl + "CheckMedia"))
+                .then(function (data){
+                   vm.mediaCheckResults = JSON.parse(data);
+                });
+        }
+
         vm.toggle = function (propName){
             let currentValue = vm.settings[propName];
             vm.settings[propName] = !currentValue;
         };
-        
+
         $scope.setProperty = function (propName, value){
             vm.settings[propName] = value;
         };
 
         function humanFileSize(bytes, si=false, dp=1) {
             const thresh = si ? 1000 : 1024;
-            
+
             if (Math.abs(bytes) < thresh) {
                 return bytes + ' B';
             }
-            
+
             const units = si
                 ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
                 : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
-            
+
             let u = -1;
             const r = 10**dp;
-            
+
             do {
                 bytes /= thresh;
                 ++u;
@@ -53,9 +70,6 @@ function WebPicAutoController($scope, $http, umbRequestHelper) {
 
     }
     init();
-    
-
-
 }
 
 angular.module("umbraco").controller("Badgernet.Umbraco.WebPicAutoController", WebPicAutoController);
---
+++ Badgernet.Umbraco.TestingProject/Controllers/WebPicAutoController.cs
@@ -0,0 +1,120 @@
+﻿using Badgernet.WebPicAuto.Settings;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using SixLabors.ImageSharp;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.BackOffice.Controllers;
+using Umbraco.Cms.Web.Common;
+using Umbraco.Cms.Web.Common.Authorization;
+using uSync.Core;
+
+namespace Badgernet.WebPicAuto.Controllers
+{
+    [Authorize(Policy = AuthorizationPolicies.SectionAccessSettings)]
+    public class WebPicAutoController : UmbracoAuthorizedJsonController
+    {
+        private readonly IWpaSettingsProvider _settingsProvider;
+        private readonly ILogger<WebPicAutoController> _logger;
+        private readonly IUmbracoContextAccessor _contextAccessor;
+        private WpaSettings? _currentSettings;
+
+        public WebPicAutoController(IWpaSettingsProvider settingsProvider, ILogger<WebPicAutoController> logger, IUmbracoContextAccessor contextAccessor)
+        {
+            _settingsProvider = settingsProvider;
+            _logger = logger;
+            _contextAccessor = contextAccessor;
+        }
+
+        public string GetSettings()
+        {
+            _currentSettings ??= _settingsProvider.GetSettings();
+            return JsonConvert.SerializeObject(_currentSettings);
+        }
+
+        public string SetSettings(JObject settingsJson)
+        {
+            try
+            {
+                _currentSettings = settingsJson.ToObject<WpaSettings>();
+                _settingsProvider.PersistToFile(_currentSettings!);
+                
+                return "Settings were saved.";
+            }
+            catch(Exception e)
+            {
+                _logger.LogError("Error when saving wpa settings: {0}", e.Message);
+                return "Something went wrong, check logs.";
+            }
+        }
+        
+        private IEnumerable<ImageInfo>? GetAllImages()
+        {
+            if (_contextAccessor .TryGetUmbracoContext(out var context) == false)
+                return null;
+      
+            if (context.Content == null)
+                return null;
+
+            var mediaRoot = context.Media!.GetAtRoot();
+
+            var images = mediaRoot.DescendantsOrSelf<IPublishedContent>()
+                .OfTypes("Image")
+                .Select(i => new ImageInfo()
+                {
+                    Id =i.Id,
+                    File = (ImageCropperValue?) (i.GetProperty("UmbracoFile")?.GetValue() ?? null),
+                    Width = (int) (i.GetProperty("UmbracoWidth")?.GetValue() ?? 0),
+                    Height = (int) (i.GetProperty("UmbracoHeight")?.GetValue() ?? 0),
+                    Extension = (string) (i.GetProperty("UmbracoExtension")?.GetValue() ?? string.Empty)
+                });
+
+            return images;
+        }
+        
+        public string CheckMedia()
+        {
+            var allImages = GetAllImages();
+            if (allImages == null) return string.Empty;
+
+            _currentSettings ??= _settingsProvider.GetSettings();
+
+            var optimizeCandidates = allImages.Where(img =>
+                    (img.Width > _currentSettings.WpaTargetWidth || img.Height > _currentSettings.WpaTargetHeight) ||
+                    (img.Extension != "webp" && img.Extension != "svg"))
+                    .Select(img => img.Path);
+
+            var toResizeCount = allImages.Count(img =>
+                img.Width > _currentSettings.WpaTargetWidth || img.Height > _currentSettings.WpaTargetHeight);    
+                
+            var toConvertCount = allImages.Count(img => 
+                    img.Extension != "webp" && img.Extension != "svg");
+
+            var result = new
+            {
+                toConvertCount = toConvertCount,
+                toResizeCount = toResizeCount,
+                optimizeCandidates = optimizeCandidates
+            };
+            
+            return JsonConvert.SerializeObject(result);
+        }
+    }
+
+    public class ImageInfo()
+    {
+        public int Id { get; init; }
+        
+        public ImageCropperValue? File { get; init; }
+        public int Width { get; init; }
+        public int Height { get; init; }
+        public string Extension { get; init; }
+        public string? Path => File?.Src;
+    }
+
+}
